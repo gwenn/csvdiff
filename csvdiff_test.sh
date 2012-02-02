e76de6a189f4645517d39c1ac35be5e04f521b20
gwenn <gtreguier@gmail.com>
2012-02-02 16:32:52 +0100

    Use GOPATH in test script.

--- csvdiff_test.sh
+++ csvdiff_test.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-./csvdiff -k=1 testA.csv testB.csv
+$GOPATH/bin/csvdiff -k=1 testA.csv testB.csv
 
 echo
 echo "Test with gzipped files..."
 gzip -c testA.csv > testA.csv.gz
 gzip -c testB.csv > testB.csv.gz
 
-./csvdiff -k=1 testA.csv.gz testB.csv.gz
+$GOPATH/bin/csvdiff -k=1 testA.csv.gz testB.csv.gz
 
 rm testA.csv.gz testB.csv.gz
 
@@ -14,16 +14,16 @@ echo
 echo "Test with pipe separator..."
 tr ',' '|' < testA.csv > testA.tsv
 tr ',' '|' < testB.csv > testB.tsv
-./csvdiff -s=\| -k=1 -f=1 testA.tsv testB.tsv
+$GOPATH/bin/csvdiff -s=\| -k=1 -f=1 testA.tsv testB.tsv
 rm testA.tsv testB.tsv
 
 echo
 echo "Test with ignored field..."
-./csvdiff -k=1 -i=2 testA.csv testB.csv
+$GOPATH/bin/csvdiff -k=1 -i=2 testA.csv testB.csv
 
 echo
 echo "Test with guessed separator..."
 tr ',' ';' < testA.csv > testA.dsv
 tr ',' ';' < testB.csv > testB.dsv
-./csvdiff -k=1 testA.dsv testB.dsv
+$GOPATH/bin/csvdiff -k=1 testA.dsv testB.dsv
 rm testA.dsv testB.dsv
