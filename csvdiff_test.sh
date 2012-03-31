e38d60f94af244ae1c3af398c43b8737f332675d
gwenn <gtreguier@gmail.com>
2012-03-31 09:23:53 +0200

    Use 'go run'.

--- csvdiff_test.sh
+++ csvdiff_test.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-$GOPATH/bin/csvdiff -k=1 testA.csv testB.csv
+go run csvdiff.go -k=1 testA.csv testB.csv
 
 echo
 echo "Test with gzipped files..."
 gzip -c testA.csv > testA.csv.gz
 gzip -c testB.csv > testB.csv.gz
 
-$GOPATH/bin/csvdiff -k=1 testA.csv.gz testB.csv.gz
+go run csvdiff.go -k=1 testA.csv.gz testB.csv.gz
 
 rm testA.csv.gz testB.csv.gz
 
@@ -14,16 +14,16 @@ echo
 echo "Test with pipe separator..."
 tr ',' '|' < testA.csv > testA.tsv
 tr ',' '|' < testB.csv > testB.tsv
-$GOPATH/bin/csvdiff -s=\| -k=1 -f=1 testA.tsv testB.tsv
+go run csvdiff.go -s=\| -k=1 -f=1 testA.tsv testB.tsv
 rm testA.tsv testB.tsv
 
 echo
 echo "Test with ignored field..."
-$GOPATH/bin/csvdiff -k=1 -i=2 testA.csv testB.csv
+go run csvdiff.go -k=1 -i=2 testA.csv testB.csv
 
 echo
 echo "Test with guessed separator..."
 tr ',' ';' < testA.csv > testA.dsv
 tr ',' ';' < testB.csv > testB.dsv
-$GOPATH/bin/csvdiff -k=1 testA.dsv testB.dsv
+go run csvdiff.go -k=1 testA.dsv testB.dsv
 rm testA.dsv testB.dsv
